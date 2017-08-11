6fa9f83b00db13165dd70dd5b7266fad673709a9
Ryan Templin <tempjuice@hotmail.com>
2017-08-10 22:02:00 -0500

    Added remaining array values to the database.

--- server/server.js
+++ server/server.js
@@ -12,35 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = 5000;
 
-// Creating jokes array
-var jokes = [
-  {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat"
-  },
-  {
-    whoseJoke: "Luke",
-    jokeQuestion: "Twofish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
-  },
-  {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
-  },
-  {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs"
-  },
-  {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog,",
-    punchLine: "It was a shih tzu."
-  }
-];
-
 app.use('/joke', joke);
 
 // //Post Route
